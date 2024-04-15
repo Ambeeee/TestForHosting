dfee1473f71e387c0f9c4e27b9d3b2f70a1ec772
Ambeeee <77481329+Ambeeee@users.noreply.github.com>
2024-04-15 17:40:51 +0200

    V3
    starting...

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Deploy Me!</h1><style>body{display:flex; align-items:center; justify-content:center: height:}</style>'
+    return '<h1>Deploy Me!</h1><style>body{display:flex; align-items:center; justify-content:center; height:100vh;}</style>'
 
 
